0984773d8a9a97666399f339cec6f56663dd9275
Paul Evans <evansp2@gmail.com>
2015-06-02 10:18:28 -0400

    updating to use latest deps

--- project.clj
+++ project.clj
@@ -12,15 +12,16 @@
                  [compojure "1.2.1"]
                  [liberator "0.12.2"]
                  [pe-rest-utils "0.0.9"]
-                 [pe-user-core "0.1.9"]
+                 [pe-user-core "0.1.10"]
                  [pe-fp-core "0.0.7"]
                  [pe-core-utils "0.0.11"]
+                 [pe-jdbc-utils "0.0.3"]
                  [ch.qos.logback/logback-classic "1.0.13"]
                  [org.slf4j/slf4j-api "1.7.5"]
                  [clj-time "0.8.0"]]
   :resource-paths ["resources"]
   :codox {:exclude [user]
-          :src-dir-uri "https://github.com/evanspa/pe-fp-rest/blob/0.0.4/"
+          :src-dir-uri "https://github.com/evanspa/pe-fp-rest/blob/0.0.5/"
           :src-linenum-anchor-prefix "L"}
   :profiles {:dev {:source-paths ["dev"]  ;ensures 'user.clj' gets auto-loaded
                    :plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
@@ -30,7 +31,7 @@
                                   [org.clojure/java.classpath "0.2.2"]
                                   [org.clojure/tools.nrepl "0.2.7"]
                                   [org.clojure/data.json "0.2.5"]
-                                  [pe-user-rest "0.0.13"]
+                                  [pe-user-rest "0.0.15"]
                                   [pe-rest-testutils "0.0.5"]
                                   [org.postgresql/postgresql "9.4-1201-jdbc41"]
                                   [ring-server "0.3.1"]
