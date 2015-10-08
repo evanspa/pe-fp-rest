4a60132fece16096ab35e1e9ccbd4715f5715d3e
Paul Evans <evansp2@gmail.com>
2015-10-08 10:26:06 -0400

    Version 0.0.29-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pe-fp-rest "0.0.28"
+(defproject pe-fp-rest "0.0.29-SNAPSHOT"
   :description "A Clojure library providing a REST API interface on top of the fuel purchase abstractions."
   :url "https://github.com/evanspa/pe-fp-rest"
   :license {:name "MIT"
