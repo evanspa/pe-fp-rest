3af40119e235b9acf16bcf6c24e8c43fdcdb7f05
Paul Evans <evansp2@gmail.com>
2016-07-09 00:25:21 -0400

    Version 0.0.44-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pe-fp-rest "0.0.43"
+(defproject pe-fp-rest "0.0.44-SNAPSHOT"
   :description "A Clojure library providing a REST API interface on top of the Gas Jot abstractions."
   :url "https://github.com/evanspa/pe-fp-rest"
   :license {:name "MIT"
